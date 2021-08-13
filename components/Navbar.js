d2bbfff6f92484502fe00fe07770208a15330070
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-13 22:04:02 +0700

    make navbar with responsive

---
+++ components/Navbar.js
@@ -0,0 +1,72 @@
+import Image from "next/image"
+import Link from 'next/link'
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  return (
+    <div className="xs:container sm:container md:container lg:container xl:container mx-auto">
+      <nav className="flex flex-col lg:flex-row justify-between items-center sm:h-20 md:h-28 lg:h-28">
+        <div className="flex flex-row justify-between items-center w-full lg:w-auto border-b lg:border-b-0">
+          <div>
+            <Link href="/">
+              <a>
+                <Image src="/logo.jpg" alt="logo" width="41px" height="41px" />
+              </a>
+            </Link>
+          </div>
+
+          <div>
+            <button onClick={() => setIsMobile(!isMobile)} className="focus:outline-none text-black block lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {!isMobile ? (
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                ) : (
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className={`${isMobile === true ? 'block' : 'hidden'} lg:block ml-auto`}>
+          <ul className="flex flex-col lg:flex-row justify-between items-center">
+            <li className="mt-5 lg:mt-0 lg:mr-8 text-gray-600 hover:text-black focus:text-black text-base">
+              <Link href="/">
+                Home
+              </Link>
+            </li>
+            <li className="mt-5 lg:mt-0 lg:mr-8 text-gray-600 hover:text-black focus:text-black text-base">
+              <Link href="/category">
+                Vehicle Type
+              </Link>
+            </li>
+            <li className="mt-5 lg:mt-0 lg:mr-8 text-gray-600 hover:text-black focus:text-black text-base">
+              <Link href="/history">
+                History
+              </Link>
+            </li>
+            <li className="mt-5 lg:mt-0 lg:mr-10 text-gray-600 hover:text-black focus:text-black text-base">
+              <Link href="/about">
+                About
+              </Link>
+            </li>
+            <li className="mt-5 lg:mt-0 lg:mr-5 border border-yellow-400 hover:bg-yellow-400 focus:bg-yellow-400 text-base w-28 h-10 flex items-center justify-center rounded-md">
+              <Link href="/login">
+                Login
+              </Link>
+            </li>
+            <li className="mt-5 lg:mt-0 border border-yellow-400 hover:bg-yellow-400 focus:bg-yellow-400 text-base w-28 h-10 flex items-center justify-center rounded-md">
+              <Link href="/register">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      'xs': '350px',
+      // => @media (max-width: 640px) { ... }
+
+      'sm': '576px',
+      // => @media (min-width: 640px) { ... } 
+  
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+  
+      'lg': '992px',
+      // => @media (min-width: 1024px) { ... }
+  
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+    }
   },
   variants: {
     extend: {},
