7eb758a2235a0a016f21eb7d271063fc391b09d6
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-14 16:42:28 +0700

    update font

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish&family=Nunito:wght@400;600;800;900&family=Playfair+Display:wght@400;700;900&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
