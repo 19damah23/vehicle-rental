8a8f478505b7453ffea2e2a4c8f92fc92174f7d2
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-14 14:07:46 +0700

    make star component

---
+++ components/Star.js
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+const Star = () => {
+  return (
+    <div className="mr-4">
+      <Image src="/star.png" alt="star" width="25px" height="25px" />
+    </div>
+  );
+}
+
+export default Star;
