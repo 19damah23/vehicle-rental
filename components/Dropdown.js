ff7c079207933c1479e1c9dab81371b97bf8aa11
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-18 23:25:50 +0700

    make feature add new vehicle

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,30 +1,19 @@
 import { useState } from "react";
 
-const Dropdown = ({ list, giveClass, classBody, classList, classSelect }) => {
-  const [show, setShow] = useState(false)
-  const [selected, setSelected] = useState(list[0])
+const Dropdown = ({ list, classSelect, title, titleClass, handleChange, name, id }) => {
 
   return (
-    <div className={`relative ${giveClass}`}>
-      <button className={`border flex flex-row justify-between items-center pl-1 ${classBody ? classBody : ''}`} onClick={() => setShow(!show)}>
-        <p className={classSelect}>{selected}</p>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          {!show ? (
-            <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-          ) : (
-            <path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" />
-          )}
-        </svg>
-      </button>
-
-      <div className={`${show ? 'block' : 'hidden'} absolute z-10 w-48 bg-white py-2 rounded-lg mt-2 shadow-md ${classList ? classList : ''}`}>
+    <>
+      {title ? (
+        <label htmlFor={id} className={titleClass}>{title}</label>
+      ) : ''}
+      <select name={name} id={id} onChange={handleChange} className={classSelect}>
         {list && list.map((item, index) => (
-          <li onClick={() => setSelected(item, setShow(!show))} className="list-none px-4 py-2 hover:bg-indigo-500 hover:text-white" key={index}>
-            <a>{item}</a>
-          </li>
+          <option value={item} key={index}>{item}</option>
         ))}
-      </div>
-    </div>
+      </select>
+    </>
+
   );
 }
 
--- pages/admin/add/index.js
+++ pages/admin/add/index.js
@@ -2,78 +2,154 @@ import Navbar from "../../../components/Navbar"
 import Image from "next/image"
 import Footer from "../../../components/Footer"
 import Dropdown from "../../../components/Dropdown"
+import { useState } from "react"
+import backendApi from "../../api/backendApi"
 
 const AddVehicle = () => {
+  const [stock, setStock] = useState(1)
+  const [images, setImages] = useState([]);
+  const [imagesPreview] = [images.map((item) => URL.createObjectURL(item))]
+  const [input, setInput] = useState({
+    name: '',
+    location: '',
+    description: '',
+    price: null,
+    stock: stock,
+    category: 'cars',
+    status: 'available'
+  })
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    console.log(e.target.name, e.target.value)
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onFileChange = (e) => {
+    setImages([...e.target.files]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const files = document.querySelector('input[type="file"]').files
+    const data = new FormData();
+    data.append('name', input.name)
+    data.append('location', input.location)
+    data.append('description', input.description)
+    data.append('price', input.price)
+    data.append('stock', input.stock)
+    data.append('category', input.category)
+    data.append('status', input.status)
+    for (let i = 0; i < files.length; i++) {
+      data.append('images', files[i])
+    };
+
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxNjlkZDFlMGQwOTRmMDI4ZjMwZGVlZGM1OWJiNjY5IiwibmFtZSI6Ik11Y2hhbWFkIEFndXMgSGVybWF3YW4iLCJlbWFpbCI6Im11Y2hhbWFkYWd1c2hAZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwicGhvbmUiOm51bGwsImF2YXRhciI6bnVsbCwiZ2VuZGVyIjpudWxsLCJleHAiOjE2MjkyOTk3OTMsImlhdCI6MTYyOTI5NjE5M30.IXoTBWckqZ6zY0PXCkASZGs9LzVkDrYImv-p_2MUbP8'
+    const config = {
+      headers: { Authorization: `Bearer ${token}` }
+    };
+
+    const res = await backendApi.post(`vehicles/add`, data, config)
+    console.log(res)
+  };
+
+  console.log(imagesPreview)
+
   return (
     <>
       <Navbar />
 
       <div className="xs:container sm:container md:container lg:container xl:container mx-auto flex flex-col py-20 lg:pt-32 lg:pb-24">
-        <div className="flex items-center">
-          <button className="w-5 h-10 lg:w-7 lg:h-12">
-            <Image src="/prev.png" alt="back" width="28px" height="48px" />
-          </button>
-          <h3 className="text-xl lg:text-4xl font-bold ml-12 lg:ml-20">Add new item</h3>
-        </div>
+        <form onSubmit={handleSubmit} encType="multipart/form-data">
+          <div className="flex items-center">
+            <button className="w-5 h-10 lg:w-7 lg:h-12">
+              <Image src="/prev.png" alt="back" width="28px" height="48px" />
+            </button>
+            <h3 className="text-xl lg:text-4xl font-bold ml-12 lg:ml-20">Add new item</h3>
+          </div>
 
-        <div className="flex flex-col lg:flex-row justify-between mt-10 lg:mt-24">
-          <div className="w-full lg:w-1/2 pr-0 lg:pr-5">
-            <div className="flex flex-col justify-center items-center w-full h-48 lg:h-96 bg-gray-200 rounded-lg">
-              <div className="h-16 w-20 lg:h-28 lg:w-28">
-                <Image src="/camera.png" alt="camera" height="110px" width="130px" />
+          <div className="flex flex-col lg:flex-row justify-between mt-10 lg:mt-24">
+            <div className="w-full lg:w-1/2 pr-0 lg:pr-5">
+              <div className="flex flex-col justify-center items-center w-full h-48 lg:h-96 bg-gray-200 rounded-lg">
+                {imagesPreview[0] ? (
+                  <img src={imagesPreview[0]} alt="camera" className="w-full h-full rounded-md" />
+                ) : (
+                  <label htmlFor="images" className="flex items-center flex-col">
+                    <div className="h-16 w-20 lg:h-28 lg:w-28">
+                      <Image src="/camera.png" alt="camera" height="110px" width="130px" />
+                    </div>
+                    <p className="text-gray-400 mt-5">Click to add image</p>
+                  </label>
+                )}
               </div>
-              <p className="text-gray-400 mt-5">Click to add image</p>
-            </div>
 
-            <div className="flex justify-between mt-5">
-              <div className="flex flex-col justify-center items-center w-full h-20 lg:h-44 bg-gray-200 rounded-lg mr-2 lg:mr-5">
-                <div className="w-6 h-6 lg:w-12 lg:h-12">
-                  <Image src="/camera.png" alt="camera" height="65px" width="68px" />
+              {imagesPreview[1] ? (
+                <div className="flex justify-between mt-5 overflow-x-scroll">{
+                imagesPreview.map((item) => (
+                    <div className="flex flex-col justify-center items-center w-full h-20 lg:h-44 bg-gray-200 rounded-lg mr-2 lg:mr-5">
+                      <img src={item} alt="camera" className="w-full h-full rounded-md" />
+                    </div>
+                ))
+                }</div>
+              ) : (
+                <div className="flex justify-between mt-5">
+                  <div className="flex flex-col justify-center items-center w-full h-20 lg:h-44 bg-gray-200 rounded-lg mr-2 lg:mr-5">
+                    <label htmlFor="images" className="flex items-center flex-col">
+                      <div className="w-6 h-6 lg:w-12 lg:h-12">
+                        <Image src="/camera.png" alt="camera" height="65px" width="68px" />
+                      </div>
+                      <p className="text-gray-400 mt-2 lg:mt-5">Click to add image</p>
+                    </label>
+                  </div>
+                  <div className="flex flex-col justify-center items-center w-full h-20 lg:h-44 bg-gray-200 rounded-lg ml-2 lg:ml-5">
+                    <label htmlFor="images" className="flex items-center flex-col">
+                      <div className="w-6 h-6 lg:w-12 lg:h-12">
+                        <Image src="/plusgray.png" alt="plus" height="65px" width="68px" />
+                      </div>
+                      <p className="text-gray-400 mt-2 lg:mt-5">Add more</p>
+                    </label>
+                  </div>
                 </div>
-                <p className="text-gray-400 mt-2 lg:mt-5">Click to add image</p>
+              )}
+              <input type="file" name="images" id="images" multiple className="hidden" onChange={(e) => onFileChange(e)} accept="image/jpeg, image/png, image/jpg" />
+            </div>
+            <div className="w-full lg:w-1/2 pl-0 lg:pl-5">
+              <input type="text" name="name" placeholder="Name (max up to 50 words)" className="border-b text-gray-400 font-light text-lg lg:text-2xl w-full py-4 focus:outline-none" onChange={handleInput} />
+              <input type="text" name="location" placeholder="Location" className="border-b text-gray-400 font-light text-lg lg:text-2xl w-full py-4 focus:outline-none" onChange={handleInput} />
+              <input type="text" name="description" placeholder="Description (max up to 150 words)" className="border-b text-gray-400 font-light text-lg lg:text-2xl w-full py-4 focus:outline-none" onChange={handleInput} />
+              <div className="flex flex-col mt-4">
+                <label htmlFor="price" className="fontPlayfair font-bold text-lg lg:text-2xl mt-2">Price :</label>
+                <input type="text" name="price" id="price" placeholder="Type the price" className="px-4 py-3 lg:py-4 rounded-md bg-gray-200 text-gray-400 text-lg lg:text-2xl focus:outline-none mt-2 lg:mt-4" onChange={handleInput} />
               </div>
-              <div className="flex flex-col justify-center items-center w-full h-20 lg:h-44 bg-gray-200 rounded-lg ml-2 lg:ml-5">
-                <div className="w-6 h-6 lg:w-12 lg:h-12">
-                  <Image src="/plusgray.png" alt="plus" height="65px" width="68px" />
+              <div className="flex flex-col mt-4">
+                <Dropdown title="Status :" name="status" id="status" list={['available', 'full booked']} handleChange={handleInput} titleClass="fontPlayfair font-bold text-lg lg:text-2xl mt-2" classSelect="px-4 py-3 lg:py-4 rounded-md bg-gray-200 text-gray-400 text-lg lg:text-2xl focus:outline-none mt-2 lg:mt-4" />
+              </div>
+              <div className="flex justify-between items-center mt-4 lgmt-6">
+                <label htmlFor="stock" className="fontPlayfair font-bold text-lg lg:text-2xl">Stock :</label>
+                <div className="flex items-center justify-between">
+                  <button type="button" className="px-2 py-2 w-8 h-8 bg-yellow-400 hover:bg-yellow-500 rounded-md" onClick={() => setStock(stock + 1)}>
+                    <Image src="/plus.png" alt="plus" height="20px" width="20px" />
+                  </button>
+                  <span className="text-2xl font-black mx-8 lg:mx-16">{stock}</span>
+                  <button type="button" className="px-2 w-8 h-8 bg-gray-300 hover:bg-gray-400 rounded-md flex items-center" onClick={() => setStock(stock > 1 ? stock - 1 : 1)}>
+                    <Image src="/minus.png" alt="minus" height="8px" width="20px" />
+                  </button>
                 </div>
-                <p className="text-gray-400 mt-2 lg:mt-5">Add more</p>
               </div>
             </div>
           </div>
-          <div className="w-full lg:w-1/2 pl-0 lg:pl-5">
-            <input type="text" name="" placeholder="Name (max up to 50 words)" className="border-b text-gray-400 font-light text-lg lg:text-2xl w-full py-4 focus:outline-none" />
-            <input type="text" name="" placeholder="Location" className="border-b text-gray-400 font-light text-lg lg:text-2xl w-full py-4 focus:outline-none" />
-            <input type="text" name="" placeholder="Description (max up to 150 words)" className="border-b text-gray-400 font-light text-lg lg:text-2xl w-full py-4 focus:outline-none" />
-            <div className="flex flex-col mt-4">
-              <label htmlFor="price" className="fontPlayfair font-bold text-lg lg:text-2xl mt-2">Price :</label>
-              <input type="text" name="price" id="price" placeholder="Type the price" className="px-4 py-3 lg:py-4 rounded-md bg-gray-200 text-gray-400 text-lg lg:text-2xl focus:outline-none mt-2 lg:mt-4" />
+          <div className="flex justify-between mt-16">
+            <div className="w-2/5 pr-2 lg:pr-4">
+              <Dropdown list={["Cars", "Bike", "Motorbike", "+  Add category"]} name="category" id="category" handleChange={handleInput} classSelect="px-4 w-full h-12 lg:h-20 rounded-md text-white text-lg lg:text-2xl focus:outline-none bg-black font-bold" />
             </div>
-            <div className="flex flex-col mt-4">
-              <label htmlFor="status" className="fontPlayfair font-bold text-lg lg:text-2xl mt-2">Status :</label>
-              <Dropdown list={["Available", "Full booked"]} classBody="w-full pl-4 py-3 lg:py-4 bg-gray-200 text-gray-400 mt-2 lg:mt-4 rounded-md" classSelect="pl-4 text-lg lg:text-2xl" />
-            </div>
-            <div className="flex justify-between items-center mt-4 lgmt-6">
-              <label htmlFor="stock" className="fontPlayfair font-bold text-lg lg:text-2xl">Stock :</label>
-              <div className="flex items-center justify-between">
-                <button className="px-2 py-2 w-8 h-8 bg-yellow-400 hover:bg-yellow-500 rounded-md">
-                  <Image src="/plus.png" alt="plus" height="20px" width="20px" />
-                </button>
-                <span className="text-2xl font-black mx-8 lg:mx-16">2</span>
-                <button className="px-2 w-8 h-8 bg-gray-300 hover:bg-gray-400 rounded-md flex items-center">
-                  <Image src="/minus.png" alt="minus" height="8px" width="20px" />
-                </button>
-              </div>
+            <div className="w-3/5 pl-2 lg:pl-4">
+              <button className="w-full bg-yellow-400 hover:bg-yellow-500 text-2xl h-12 lg:h-20 rounded-lg font-bold" type="submit">Save item</button>
             </div>
           </div>
-        </div>
-        <div className="flex justify-between mt-16">
-          <div className="w-2/5 pr-4">
-            <Dropdown list={["Cars", "Bike", "Motorbike", "+  Add category"]} classBody="bg-black py-3 lg:py-7 w-full text-white pr-4 rounded-lg" classSelect="text-2xl text-white pl-4 font-bold" />
-          </div>
-          <div className="w-3/5 pl-4">
-            <button className="w-full bg-yellow-400 hover:bg-yellow-500 text-2xl py:3 lg:py-7 rounded-lg font-bold">Save item</button>
-          </div>
-        </div>
+        </form>
       </div>
       <Footer />
     </>
