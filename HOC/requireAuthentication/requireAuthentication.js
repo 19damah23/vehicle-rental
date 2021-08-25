d6d3638719f434a0b090846105b6bdaf8578367c
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-25 23:04:21 +0700

    setting protected routes

---
+++ HOC/requireAuthentication/requireAuthentication.js
@@ -0,0 +1,18 @@
+import cookies from "next-cookies";
+
+export function requireAuthentication(gssp) {
+  return async (context) => {
+    const token = cookies(context).token
+
+    if (!token) {
+      return {
+        redirect: {
+          destination: '/login',
+          statusCode: 302
+        }
+      };
+    }
+
+    return await gssp(context);
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import 'tailwindcss/tailwind.css'
-import Head from 'next/head'
-import '../styles/index.css'
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,11 +8,14 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Mulish&family=Nunito:wght@400;600;800;900&family=Playfair+Display:wght@400;700;900&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish&family=Nunito:wght@400;600;800;900&family=Playfair+Display:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
