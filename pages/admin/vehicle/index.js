980696ba845eb9d2f15f41a8dc51ecc99fe0040b
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-26 10:01:07 +0700

    make page vehicle admin

--- pages/admin/vehicle/index.js
+++ pages/admin/vehicle/index.js
@@ -13,7 +13,6 @@ const Vehicle = ({ data }) => {
   useEffect(() => {
     backendApi.get(`vehicles?search=${query}`)
       .then((res) => {
-        console.log(res)
         setSearchData(res.data.data)
       })
       .catch((err) => {
@@ -27,6 +26,7 @@ const Vehicle = ({ data }) => {
     setQuery(e.target.value)
   }
 
+  console.log(data)
   return (
     <>
       <Navbar />
@@ -51,109 +51,42 @@ const Vehicle = ({ data }) => {
             </Link>
           </div>
 
-            <div className="flex mt-8 mb-10 w-full flex-wrap">
-          {searchData.map((item, index) =>
+          <div className="flex mt-8 mb-10 w-full flex-wrap">
+            {searchData.map((item, index) =>
               <Link href={`/admin/vehicle/${item.name}/${item.id}`}>
                 <a className="mr-4">
-                  <Card name={item.name} location={item.location} img={`http://localhost:8080/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={index} />
+                  <Card name={item.name} location={item.location} img={`http://localhost:4000/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={index} />
                 </a>
               </Link>
-          )}
-            </div>
+            )}
+          </div>
         </div>
       ) : (
         <div>
-          <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20">
-            <div className="flex justify-between">
-              <h3 className="font-bold text-4xl fontPlayfair">Popular in town</h3>
-              <Link href="/admin/vehicle/popular" className="text-yellow-400 flex items-center">
-                <a className="text-yellow-400 flex items-center">
-                  Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
-              </Link>
-            </div>
-
-            <div className="flex mt-8 w-full flex-wrap">
-              {data && data.map((item, index) => (
-                <Link href={`/admin/vehicle/${item.name}/${item.id}`}>
-                  <a className="mx-3 lg:mx-1">
-                    <Card name={item.name} location={item.location} img={`http://localhost:8080/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={index} />
+          {data && data.map((items, index) => (
+            <div className="xs:container sm:container md:container lg:container xl:container mx-auto py-10" key={index}>
+              <div className="flex justify-between">
+                <h3 className="font-bold text-4xl fontPlayfair">{items[0].category}</h3>
+                <Link href="/admin/vehicle/popular" className="text-yellow-400 flex items-center">
+                  <a className="text-yellow-400 flex items-center">
+                    Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                    </svg>
                   </a>
                 </Link>
-              ))}
-            </div>
-          </div>
+              </div>
 
-          <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20">
-            <div className="flex justify-between">
-              <h3 className="font-bold text-4xl fontPlayfair">Cars</h3>
-              <Link href="/admin/vehicle/popular" className="text-yellow-400 flex items-center">
-                <a className="text-yellow-400 flex items-center">
-                  Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
-              </Link>
+              <div className="flex mt-8 w-full flex-wrap">
+                {items.map((item) => (
+                  <Link href={`/admin/vehicle/${item.name}/${item.id}`}>
+                    <a className="mx-3 lg:mx-0 lg:mr-3">
+                      <Card name={item.name} location={item.location} img={`http://localhost:4000/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={item.id} />
+                    </a>
+                  </Link>
+                ))}
+              </div>
             </div>
-
-            <div className="flex mt-8 w-full flex-wrap">
-              {data && data.map((item, index) => (
-                <Link href={`/admin/vehicle/${item.name}/${item.id}`}>
-                  <a className="mx-3 lg:mx-0 lg:mr-3">
-                    <Card name={item.name} location={item.location} img={`http://localhost:8080/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={index} />
-                  </a>
-                </Link>
-              ))}
-            </div>
-          </div>
-
-          <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20">
-            <div className="flex justify-between">
-              <h3 className="font-bold text-4xl fontPlayfair">Motorbike</h3>
-              <Link href="/admin/vehicle/popular" className="text-yellow-400 flex items-center">
-                <a className="text-yellow-400 flex items-center">
-                  Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
-              </Link>
-            </div>
-
-            <div className="flex mt-8 w-full flex-wrap">
-              {data && data.map((item, index) => (
-                <Link href={`/admin/vehicle/${item.name}/${item.id}`}>
-                  <a className="mx-3 lg:mx-0 lg:mr-3">
-                    <Card name={item.name} location={item.location} img={`http://localhost:8080/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={index} />
-                  </a>
-                </Link>
-              ))}
-            </div>
-          </div>
-
-          <div className="xs:container sm:container md:container lg:container xl:container mx-auto py-20">
-            <div className="flex justify-between">
-              <h3 className="font-bold text-4xl fontPlayfair">Bike</h3>
-              <Link href="/admin/vehicle/popular" className="text-yellow-400 flex items-center">
-                <a className="text-yellow-400 flex items-center">
-                  Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
-              </Link>
-            </div>
-
-            <div className="flex mt-8 w-full flex-wrap">
-              {data && data.map((item, index) => (
-                <Link href={`/admin/vehicle/${item.name}/${item.id}`}>
-                  <a className="mx-3 lg:mx-0 lg:mr-3">
-                    <Card name={item.name} location={item.location} img={`http://localhost:8080/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={index} />
-                  </a>
-                </Link>
-              ))}
-            </div>
-          </div>
+          ))}
         </div>
       )}
 
@@ -163,7 +96,7 @@ const Vehicle = ({ data }) => {
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.NEXT_BACKEND_API}v1/vehicles`)
+  const res = await fetch(`${process.env.NEXT_BACKEND_API}v1/vehicles/grouped`)
   const data = await res.json()
 
   return { props: data }
