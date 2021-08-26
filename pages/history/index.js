2e118083c1ed4402043af17df09503ddccfeb8c6
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-26 10:02:32 +0700

    make history transaction user

--- pages/history/index.js
+++ pages/history/index.js
@@ -2,13 +2,55 @@ import Footer from "../../components/Footer"
 import Navbar from "../../components/Navbar"
 import Image from "next/image";
 import Dropdown from "../../components/Dropdown"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "../../components/Card";
+import cookies from "next-cookies";
+import ReactPaginate from "react-paginate";
+import backendApi from "../api/backendApi";
 
-const History = () => {
-  const [filter, setFilter] = useState([
-    "Type", "Date Added", "Name", "Favorite Product"
-  ])
+const History = (context) => {
+  const [data, setData] = useState([])
+  const [totalPage, setTotalPage] = useState(0)
+  const [perPage, setPerPage] = useState(2)
+  const [page, setPage] = useState(1)
+  const [orderBy, setOrderBy] = useState("category")
+  const [sort, setSort] = useState("ASC")
+  const { userId, token } = cookies(context)
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    backendApi.get(`transactions/history/${userId}?page=${page}&perPage=${perPage}&orderBy=${orderBy}&sortBy=${sort}&search=${query}`, {
+      withCredentials: true,
+      headers: {
+        Cookie: 'token=' + token
+      }
+    })
+    .then((res) => {
+      console.log(res)
+      const meta = res.data.meta
+      const data = res.data.data
+      setTotalPage(meta.totalPage)
+      setData(data)
+    })
+    .catch((err) => {
+      console.log(err.response.data.message)
+    })
+  }, [sort, page, perPage, orderBy, query])
+
+  const handleChangePage = (e) => {
+    setPage(e.selected + 1)
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    setQuery(e.target.value)
+  }
+
+  const handleOrder = (e) => {
+    e.preventDefault()
+    setOrderBy(e.target.value)
+  }
+
   return (
     <>
       <Navbar />
@@ -17,42 +59,41 @@ const History = () => {
         <div className="w-full lg:w-3/4 mr-10">
           <div className="flex flex-row items-center">
             <div className="relative mr-4 lg:mr-9 w-full">
-              <input type="text" name="search" placeholder="Search history" className="w-full h-9 lg:h-16 border border-gray-500 px-6 text-gray-600 focus:outline-none rounded-md bg-white opacity-25" />
+              <input type="text" name="search" placeholder="Search history" className="w-full h-9 lg:h-16 border border-gray-500 px-6 text-gray-600 focus:outline-none rounded-md bg-white opacity-25" onChange={handleSearch} />
               <div className="absolute right-3 top-2 w-5 h-5 lg:w-8 lg:h-8 lg:right-6 lg:top-4">
                 <Image src="/search.png" alt="search" width="30px" height="30px" />
               </div>
             </div>
-            <Dropdown list={filter} classBody="text-black bg-white opacity-25 w-16 lg:w-24 h-8 lg:h-16 border-gray-400 text-gray-600 text-sm lg:text-xl px-2" classList="right-0" />
+            <Dropdown list={[{title: "category"}, {title: "name"}]} titleClass="font-bold text-sm lg:text-base" classSelect="rounded-md text-black bg-white border text-sm lg:text-base focus:outline-none lg:mr-4 h-16 w-60" handleChange={handleOrder} />
           </div>
-
-          <p className="text-gray-400 my-4 lg:my-8 text-2xl">Today</p>
-          <p className="text-lg lg:text-2xl my-1 lg:my-3">User 1 cancelled the payment</p>
-          <hr />
-          <p className="text-lg lg:text-2xl my-1 lg:my-3">Payment for Booking Order #12367789YZS has been finished</p>
-          <hr />
 
           <div className="flex flex-col py-8 lg:py-16">
             <p className="text-gray-400 text-2xl">A week ago</p>
-            <div className="flex flex-row justify-between mt-6 lg:mt-10 relative">
-              <button className="block lg:hidden absolute top-0 right-0">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg>
-              </button>
-              <div className="w-32 h-16 lg:w-52 lg:h-40 object-coverrounded-md">
-                <Image src="/img9.png" alt="vehicle" width="200" height="165" className="rounded-2xl" />
+            {data && data.map((item) => (
+              <div className="flex flex-row mt-6 lg:mt-10">
+                <div className="w-32 h-16 lg:w-52 lg:h-40 object-coverrounded-md">
+                  <Image src={`http://localhost:4000/files/${item.images}`} alt="vehicle" width="200" height="165" className="rounded-2xl" />
+                </div>
+                <div className="flex flex-col justify-center ml-8 lg:ml-16">
+                  <p className="text-base lg:text-2xl font-normal">{item.name}</p>
+                  <p className="text-sm lg:text-lg font-normal">{item.startDate + ' to ' + item.expDate}</p>
+                  <p className="text-base lg:text-2xl font-normal mt-2 lg:mt-4">Prepayment : Rp. {item.subTotal}</p>
+                  <p className="text-base lg:text-2xl font-normal text-green-600">{item.status}</p>
+                </div>
               </div>
-              <div className="flex flex-col justify-center lg:mr-48">
-                <p className="text-base lg:text-2xl font-normal">Vespa Matic</p>
-                <p className="text-sm lg:text-lg font-normal">Jan 18 to 21 2021</p>
-                <p className="text-base lg:text-2xl font-normal mt-2 lg:mt-4">Prepayment : Rp. 245.000</p>
-                <p className="text-base lg:text-2xl font-normal text-green-600">Finish Payment & Rreturned</p>
-              </div>
-              <div className="flex items-center">
-                <button className="bg-yellow-300 hover:bg-yellow-400 font-bold hidden lg:block lg:text-2xl lg:px-8 lg:py-2 rounded-md">Delete</button>
-              </div>
-            </div>
+            ))}
           </div>
+          <ReactPaginate
+            previousLabel={"prev"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            breakClassName={"breack-me"}
+            pageCount={totalPage}
+            marginPagesDisplayed={5}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages-pagination"}
+            activeClassName={"active"}
+            onPageChange={handleChangePage} />
         </div>
 
         <div className="w-full lg:w-1/4 flex flex-col items-center px-4 lg:px-9 py-6 lg:py-8 border border-gray-400 rounded-md mt-6 lg:mt-0">
