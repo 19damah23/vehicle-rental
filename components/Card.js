cb4e01d586e658a6d36e929eb93a45a6115c9a8e
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-09-18 20:37:35 +0700

    fix search and image on page vehicle user

--- components/Card.js
+++ components/Card.js
@@ -4,8 +4,8 @@ import Image from 'next/image'
 const Card = ({ img, name, location, giveClass, loader, index }) => {
   return (
     <div className={`relative ${giveClass || ''}`} key={index}>
-      <div className="w-36 h-40 lg:w-72 lg:h-80 mx-auto">
-        <Image loader={loader} src={img} alt="img" width="275px" height="337px" className="rounded-md" />
+      <div className="w-36 h-40 lg:w-72 lg:h-80 mx-auto object-contain">
+        <img src={img} alt="img" className="rounded-md h-full w-full object-cover" />
       </div>
       <div className="block absolute -bottom-6 lg:-bottom-6 left-0 lg:left-0 bg-white px-2 py-2 w-24 lg:w-44 rounded-r-md">
         <p className="font-semibold text-base lgtext-lg">{name}</p>
--- pages/vehicle/index.js
+++ pages/vehicle/index.js
@@ -1,94 +1,111 @@
 /* eslint-disable no-unused-vars */
 import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import Card from '../../components/Card'
 import Footer from '../../components/Footer'
+import backendApi from '../api/backendApi'
+import { requireAuthentication } from '../../HOC/requireAuthentication/requireAuthentication'
 
-const Vehicle = () => (
+const Vehicle = ({ data }) => {
+  const [query, setQuery] = useState('')
+  const [searchData, setSearchData] = useState([])
+
+  useEffect(() => {
+    backendApi.get(`vehicles?search=${query}`)
+      .then((res) => {
+        setSearchData(res.data.data)
+      })
+      .catch(() => {
+        setSearchData([])
+      })
+  }, [query])
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    setQuery(e.target.value)
+  }
+
+  return (
     <>
       <Navbar />
 
       <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20 lg:pt-40 relative">
-        <input type="text" name="search" placeholder="Search vehicle (ex. cars, cars name)" className="w-full h-16 border border-gray-400 px-6 text-gray-400 focus:outline-none rounded-md" />
+        <input type="text" name="search" placeholder="Search vehicle (ex. cars, cars name)" className="w-full h-16 border border-gray-400 px-6 text-gray-400 focus:outline-none rounded-md" onChange={handleSearch} />
         <div className="absolute right-6 top-24 lg:top-44">
           <Image src="/search.png" alt="search" width="30px" height="30px" />
         </div>
       </div>
 
-      <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20">
-        <div className="flex justify-between">
-          <h3 className="font-bold text-4xl fontPlayfair">Popular in town</h3>
-          <button className="text-yellow-400 flex items-center">
-            Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-            </svg>
-          </button>
-        </div>
+      {query.length > 0
+        ? (
+          <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20">
+            <div className="flex justify-between">
+              <h3 className="font-bold text-xl lg:text-4xl fontPlayfair">Popular in town</h3>
+              <Link href="/vehicle/popular" className="text-yellow-400 flex items-center">
+                <a className="text-yellow-400 flex items-center">
+                  Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                  </svg>
+                </a>
+              </Link>
+            </div>
 
-        <div className="flex justify-between mt-12">
-          <Card name="Merapi" location="Yogyakarta" img="/img1.png" />
-          <Card name="Teluk Bogam" location="Kalimantan" img="/img2.png" />
-          <Card name="Bromo" location="Malang" img="/img3.png" />
-          <Card name="Malioboro" location="Yogyakarta" img="/img4.png" />
-        </div>
-      </div>
+            <div className="flex mt-8 mb-10 w-full flex-wrap">
+              {searchData.map((item, index) => <Link href={`/vehicle/${item.name}/${item.id}`}>
+                <a className="mr-4">
+                  <Card name={item.name} location={item.location} img={`https://vehicle.muchamadagushermawan.online/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={index} />
+                </a>
+              </Link>)}
+            </div>
+          </div>
+          )
+        : (
+          <div>
+            {data && data.map((items, index) => (
+              <div className="xs:container sm:container md:container lg:container xl:container mx-auto py-10" key={index}>
+                <div className="flex justify-between">
+                  <h3 className="font-bold text-4xl fontPlayfair">{items[0].category}</h3>
+                  <Link href="/vehicle/popular" className="text-yellow-400 flex items-center">
+                    <a className="text-yellow-400 flex items-center">
+                      Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                      </svg>
+                    </a>
+                  </Link>
+                </div>
 
-      <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20">
-        <div className="flex justify-between">
-          <h3 className="font-bold text-4xl fontPlayfair">Cars</h3>
-          <button className="text-yellow-400 flex items-center">
-            Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-            </svg>
-          </button>
-        </div>
-
-        <div className="flex justify-between mt-12">
-          <Card name="Van" location="Yogyakarta" img="/img5.png" />
-          <Card name="Lamborghini" location="Shout Jakarta" img="/img6.png" />
-          <Card name="Jeep" location="Malang" img="/img7.png" />
-          <Card name="White Jeep" location="Kalimantan" img="/img8.png" />
-        </div>
-      </div>
-
-      <div className="xs:container sm:container md:container lg:container xl:container mx-auto pt-20">
-        <div className="flex justify-between">
-          <h3 className="font-bold text-4xl fontPlayfair">Motorbike</h3>
-          <button className="text-yellow-400 flex items-center">
-            Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-            </svg>
-          </button>
-        </div>
-
-        <div className="flex justify-between mt-12">
-          <Card name="Vespa" location="Yogyakarta" img="/img9.png" />
-          <Card name="Honda KLX" location="Kalimantan" img="/img10.png" />
-          <Card name="Honda" location="Malang" img="/img11.png" />
-          <Card name="Matic Bike" location="Yogyakarta" img="/img12.png" />
-        </div>
-      </div>
-
-      <div className="xs:container sm:container md:container lg:container xl:container mx-auto py-20">
-        <div className="flex justify-between">
-          <h3 className="font-bold text-4xl fontPlayfair">Bike</h3>
-          <button className="text-yellow-400 flex items-center">
-            Show all <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-            </svg>
-          </button>
-        </div>
-
-        <div className="flex justify-between mt-12">
-          <Card name="Fixie" location="Yogyakarta" img="/img13.png" />
-          <Card name="Sport Bike" location="Kalimantan" img="/img14.png" />
-          <Card name="Onthel" location="Malang" img="/img15.png" />
-          <Card name="Fixie Gray" location="Yogyakarta" img="/img16.png" />
-        </div>
-      </div>
+                <div className="flex mt-8 w-full flex-wrap">
+                  {items.map((item) => (
+                    <Link href={`/vehicle/${item.name}/${item.id}`}>
+                      <a className="mx-3 lg:mx-0 lg:mr-3">
+                        <Card name={item.name} location={item.location} img={`https://vehicle.muchamadagushermawan.online/files/${item.images[0]}`} giveClass="w-1/2 lg:w-1/4 my-4" index={item.id} />
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          )}
 
       <Footer />
     </>
-)
+  )
+}
 
 export default Vehicle
+
+export const getServerSideProps = requireAuthentication(
+  async (ctx) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}v1/vehicles/grouped`)
+    const data = await res.json()
+
+    return {
+      props: {
+        data
+      }
+    }
+  }
+)
